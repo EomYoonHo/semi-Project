d2c6e796834a501882b381c6f38c10562372aba7
rlwjd2000 <rlwjd7070@naver.com>
2023-12-14 17:42:46 +0900

    aaaaaa

--- semiProject/src/main/java/kr/co/green/member/controller/RegisterController.java
+++ semiProject/src/main/java/kr/co/green/member/controller/RegisterController.java
@@ -2,6 +2,8 @@ package kr.co.green.member.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -107,76 +109,69 @@ public class RegisterController extends HttpServlet {
 		// 값받기
 		String email = request.getParameter("email");
 		String pwd = request.getParameter("password");
-		String pwdCheck = request.getParameter("pwdCheck");
+		String pwdCheck = request.getParameter("passwordCheck");
 		String name = request.getParameter("name");
 		String phone = request.getParameter("phone");
 		String nickname = request.getParameter("nickname");
 
-		String salt = BCrypt.gensalt(12);
-
-		String hashedPassword = BCrypt.hashpw(pwd, salt);
-
-		//
-		MemberDTO memberDTO = new MemberDTO();
-		memberDTO.setM_email(email);
-		memberDTO.setM_pwd(hashedPassword);
-		memberDTO.setM_name(name);
-		memberDTO.setM_phone(phone);
-		memberDTO.setM_nickname(nickname);
+		// 유효성 검사
+		// 이름
+		String namePattern = "^[가-힣]{2,}$";
+		Pattern pattern = Pattern.compile(namePattern);
+		Matcher nameMatcher = pattern.matcher(name);
+		// 패스워드
+		String pwdpattern = "^(?=.*[a-zA-Z])(?=.*[@$!%*?&\\#])[A-Za-z\\d@$!%*?&\\#]{6,20}$";
+		Pattern passwordPattern = Pattern.compile(pwdpattern);
+		Matcher pwdMatcher = passwordPattern.matcher(pwd);
 
 		PointServiceImpl pointService = new PointServiceImpl();
 		MemberServiceImpl memberService = new MemberServiceImpl();
 
-		int result = memberService.memberEnroll(memberDTO);
+		int result = 0;
+		MemberDTO memberDTO = new MemberDTO();
+		String salt = BCrypt.gensalt(12);
+		String hashedPassword = BCrypt.hashpw(pwd, salt);
 
+		if (nameMatcher.matches() && pwdMatcher.matches() && pwd.equals(pwdCheck)) {
+			System.out.println("이름과 비번 적합+비밀번호확인완료");
+			memberDTO.setM_email(email);
+			memberDTO.setM_pwd(hashedPassword);
+			memberDTO.setM_name(name);
+			memberDTO.setM_phone(phone);
+			memberDTO.setM_nickname(nickname);
+			result = memberService.memberEnroll(memberDTO);
+		} else if (!nameMatcher.matches()) {
+			System.out.println("이름이 부적합합니다");
+//			validationAlert(response, "외국인은 회원으로 받지 않습니다");
+		} else if (!pwdMatcher.matches()) {
+			System.out.println("비밀번호가 부적합합니다");
+//			validationAlert(response, "비밀번호가 정책에 맞지 않습니다");
+		} else if (!pwd.equals(pwdCheck)) {
+			System.out.println("비밀번호가 일치하지 않습니다");
+//			validationAlert(response, "비밀번호가 일치하지 않습니다.");
+		} else {
+			System.out.println("문제가 있습니다");
+		}
+		// 포인트지갑만들기
 		MemberDTO pointmaker = new MemberDTO();
 		pointmaker.setM_email(email);
 		memberService.selectMember(pointmaker);
-
-		System.out.println(pointmaker.getM_no());
-		System.out.println(pointmaker.getM_email());
-		System.out.println(pointmaker.getM_pwd());
-		System.out.println(pointmaker.getM_name());
-		System.out.println(pointmaker.getM_nickname());
-
 		int m_no = pointmaker.getM_no();
 
-		// 회원가입
-		if (result == 0) {
+		if (result == 0) {// 회원가입성공여부
 			validationAlert(response, "회원가입에 실패했습니다.");
 		} else {
 			int presult = pointService.pointEnroll(m_no);
-			if (presult == 0) {
+			if (presult == 0) {// 포인트성공여부
 				System.out.println("포인트 실패");
 			} else {
-				System.out.println("포인트 만듬");
+				System.out.println("회원가입 성공");
+				System.out.println("+포인트 만듬");
+				RequestDispatcher view = request.getRequestDispatcher("/views/member/login.jsp");
+				view.forward(request, response);
 			}
-			System.out.println("회원가입 성공");
-			RequestDispatcher view = request.getRequestDispatcher("/views/member/login.jsp");
-			view.forward(request, response);
 		}
-
-//		//유효성 검사
-//		//이름
-//		String namePattern ="^[가-힣]+$";
-//		Pattern pattern = Pattern.compile(namePattern);
-//		Matcher nameMatcher=pattern.matcher(name);
-//		//패스워드
-//		String pwdpattern ="^(?=.*[a-zA-Z])(?=.*[@$!%*?&\\\\#])[A-Za-z\\\\d@$!%*?&\\\\#]{6,20}$";
-//		Pattern passwordPattern = Pattern.compile(pwdpattern);
-//		Matcher pwdMatcher = pattern.matcher(pwd);
-
-//		if(nameMatcher.matches()&&pwdMatcher.matches()) {
-
-//		}else if(!nameMatcher.matches()) {
-//			validationAlert(response,"외국인은 회원으로 받지 않습니다");
-//		}else if(!pwdMatcher.matches()) {
-//			validationAlert(response,"비밀번호가 정책에 맞지 않습니다");
-//		}
-
 	}
-
-	////////////////////////////////////////
 
 	private void validationAlert(HttpServletResponse response, String msg) throws IOException {
 		PrintWriter out = response.getWriter();
