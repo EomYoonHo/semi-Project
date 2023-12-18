d6a4376676ab1b97fb5acd6281213394ef7b256c
EomYoonHo <150765905+EomYoonHo@users.noreply.github.com>
2023-12-18 17:35:25 +0900

    Delete semiProject2 directory

--- semiProject2/src/main/java/kr/co/green/packages/model/service/PackageService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.green.packages.model.service;
-
-import kr.co.green.packages.model.dto.PackageDTO;
-
-public interface PackageService {
-	public void packageSelect(PackageDTO pack);
-}
